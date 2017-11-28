226b72a16036f937ca5d2b0cafcd7435020125f6
hw16a005 <hw16a005@oecu.jp>
2017-11-28 15:41:12 +0900

    作業D実装しました

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -4,7 +4,7 @@
 // TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)
 // TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
 // TODO: 砲台を青い壁に沿って上下に動かす。(C)
-// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)
+// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)  HW16A005  有村 泉弥
 // TODO: スコアのサイズを大きくする。(E)
 // TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
 // TODO: PlayBGM()関数を使って、BGMを再生する。(G)
@@ -32,13 +32,13 @@ void Start()
 void Update()
 {
     // 弾の発射
-    if (bulletPos.x <= -999 && Input::GetKeyDown(KeyMask::Space)) {
+    if (( bulletPos.x >320 || bulletPos.x <= -999)  && Input::GetKeyDown(KeyMask::Space)) {
         bulletPos = cannonPos + Vector2(50, 10);
     }
 
     // 弾の移動
     if (bulletPos.x > -999) {
-        bulletPos.x += 10 * Time::deltaTime;
+        bulletPos.x += 50 * Time::deltaTime;
 
         // ターゲットと弾の当たり判定
         Rect bulletRect(bulletPos, Vector2(32, 20));
